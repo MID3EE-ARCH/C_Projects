4a9a40773c5fd88f0ea7a2b2ffae4868c3d327cb
MID3EE-ARCH <realphoenix484@gmail.com>
2024-06-08 22:30:23 +0530

    Added Directory for upcoming project

---
+++ Student_Management/main.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <string.h>
+int main() {
+  printf("############################################ Student Management "
+         "System #############################################\n\n\n\n\n");
+  printf("                                  G.D DAV PUBLIC SCHOOL "
+         "BHANDARKOLA\n\n");
+  printf("                                          The Senior Block");
+  printf("\n\n\n\n");
+  printf("                                                                     "
+         " --Made by MID3EE");
+  return 0;
+}
